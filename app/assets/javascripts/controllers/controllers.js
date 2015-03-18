91548bc32f57ee9e97b640230116dff6e2234384
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-03-18 06:24:45 +0000

    Homepage layout

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -2,6 +2,7 @@ var appControllers = angular.module('appControllers', ['appServices']);
 
 appControllers.controller('homeController', ['$scope', function($scope)
 {
-    	$scope.$root.display_title = "Rubywebs";
+    	$scope.$root.display_title = "Rubywebs - No upfront cost Website Developing and Hosting";
+    	$scope.$root.metadescription = "Rubywebs - No upfront cost website developing and hosting using Ruby on Rails and AngularJS.  Includes SEO and Analytics with all plans."
     	$scope.$root.body_id = "home";
 }]);
