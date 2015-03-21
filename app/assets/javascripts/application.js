87a1ac1c9886d4be4031ac55ebd2d65ce377908a
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-03-21 06:27:05 +0000

    Layout updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require angular/angular
 //= require ng-file-upload/angular-file-upload
