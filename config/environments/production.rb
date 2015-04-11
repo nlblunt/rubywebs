3da2caa3b70d5f1fadda55856757855104a5c3ef
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-04-11 20:06:25 +0000

    Change to action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtpcorp.com',
+  port:                 2525,
+  domain:               'example.com',
+  user_name:            'nicolas.blunt@gmail.com',
+  password:             'rubyemail',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
   config.action_mailer.perform_deliveries = true
   
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
