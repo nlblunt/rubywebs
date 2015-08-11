f3588a6e6bf5d8d0d223c681f2becb5a09f7eddc
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-08-11 23:56:18 +0000

    Update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
 echo "Updating working files"
-sudo git pull origin master
+git pull origin master
 
 echo "Rebuilding assets"
-sudo rake assets:clobber
-sudo rake assets:precompile
+rake assets:clobber
+rake assets:precompile
 
 echo "Updating completed"
 sudo /etc/init.d/apache2 restart
