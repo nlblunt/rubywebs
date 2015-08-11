0bf2d02947c881a164cc84ab279b61a7e89c7b0b
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-08-11 23:47:51 +0000

    Update to update.sh

--- update.sh
+++ update.sh
@@ -2,15 +2,11 @@
 clear
 echo "Updating working files"
 sudo git pull origin master
+
 echo "Rebuilding assets"
 sudo rake assets:clobber
 sudo rake assets:precompile
-echo "Resetting permission for public folder"
-sudo chown -R daemon:daemon public
-echo "Resetting permission for tmp folder"
-sudo chown -R daemon:daemon tmp
-echo "Resetting permission for logs"
-sudo chown -R daemon:daemon log
+
 echo "Updating completed"
-sudo ~/stack/ctlscript.sh restart apache
+sudo /etc/init.d/apache2 restart
 echo "Apache restarted"
